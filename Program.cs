bcd1446df2d1e48b0b5fe3bd6204fc6a2c75ff2b
ffadelm <faysalfadelm0107@gmail.com>
2021-12-28 17:35:49 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using ServiceRest_097_Faysal_Fadel_Maulana;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
+
+namespace ServerCodeRest_097_Faysal_Fadel_Maulana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            Uri address = new Uri("http://localhost:1907/Mahasiswa");
+            WebHttpBinding binding = new WebHttpBinding();
+
+            try
+            {
+                hostObj = new ServiceHost(typeof(TI_UMY), address);
+                hostObj.AddServiceEndpoint(typeof(ITI_UMY), binding, "");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                hostObj.Description.Behaviors.Add(smb);
+                Binding mexbinding = MetadataExchangeBindings.CreateMexHttpBinding();
+                hostObj.AddServiceEndpoint(typeof(IMetadataExchange), mexbinding, "mex");
+
+                WebHttpBehavior whb = new WebHttpBehavior();
+                whb.HelpEnabled = true;
+                hostObj.Description.Endpoints[0].EndpointBehaviors.Add(whb);
+
+                hostObj.Open();
+                Console.WriteLine("Server Code is Ready .... ");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch(Exception e)
+            {
+                hostObj = null;
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
